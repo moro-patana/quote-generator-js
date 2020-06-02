5ce101ed6eedd3b91ecdea732bc6c1d3ea64c61c
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-02 16:30:40 +0300

    created some qoutes generator

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,1 +1,19 @@
-console.log('it works!');
+// create a function//
+function myQuote() {
+    
+// Store the qoutes in an array//
+const arrQuotes = [
+        ["No one has caught the cricket unless they crawl"],
+        ["It is easy to forget good things which people did to you than one thing"],
+        ["No one can do  what your mum do"],
+        ["I do not want to ruin a peace I do not owe you"],
+        ["Be the good captain ofyour ship"],
+]
+//Initialize the array of quote//
+
+let random = myQuote[Math.floor(Math.random() * quote.length)];
+// Check the quote one by one//
+console.log(myQuote());
+
+
+};
