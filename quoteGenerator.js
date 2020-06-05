6536a02082af8e66f38780e9eec72e9f47d741e5
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-05 11:23:07 +0300

    I called the function and return the newQuote inside the function

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,24 +1,29 @@
+function myQuote() {
 
 // Store the qoutes in an array/
+
   let quoteA = ["No one has caught the cricket unless they crawl",
                 "It is easy to forget good things which people did to you than one thing",
                 "No one can do  what your mum do",
                 "I do not want to ruin a peace I do not owe you",
                 "Be the good captain ofyour ship"];
-  let quoteB = ["let them go", 
-                "if only I know.", 
-                "Where is your code.", 
-                "Don't give up", 
+  let quoteB = ["let them go",
+                "if only I know.",
+                "Where is your code.",
+                "Don't give up",
                 "read carefully"];
-  let quoteC = ["did you solve it?", 
+  let quoteC = ["did you solve it?",
                 "I forget to leave the key",
-                "after lunch",
+                "It so hard",
                 "if it is possible",
                 "trust the others"
 ];
+let randomQuoteA = quoteA[Math.floor(Math.random() * quoteA.length)];
+let randomQuoteB = quoteB[Math.floor(Math.random() * quoteB.length)];
+let randomQuoteC = quoteC[Math.floor(Math.random() * quoteC.length)];
 
-
-
-};
-console.log("hello");
-
+let newQuote = `${randomQuoteA} ${randomQuoteB} ${randomQuoteC}`;
+return newQuote;
+}
+var newQuote = myQuote();
+console.log(newQuote);
