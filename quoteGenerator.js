50a1ea1e5cafe94e47dde0fc252acc51f4c489e5
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-05 08:56:12 +0300

    I created three arrays

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,19 +1,24 @@
-// create a function//
-function myQuote() {
-    
-// Store the qoutes in an array//
-const arrQuotes = [
-        ["No one has caught the cricket unless they crawl"],
-        ["It is easy to forget good things which people did to you than one thing"],
-        ["No one can do  what your mum do"],
-        ["I do not want to ruin a peace I do not owe you"],
-        ["Be the good captain ofyour ship"],
-]
-//Initialize the array of quote//
 
-let random = myQuote[Math.floor(Math.random() * quote.length)];
-// Check the quote one by one//
-console.log(myQuote());
+// Store the qoutes in an array/
+  let quoteA = ["No one has caught the cricket unless they crawl",
+                "It is easy to forget good things which people did to you than one thing",
+                "No one can do  what your mum do",
+                "I do not want to ruin a peace I do not owe you",
+                "Be the good captain ofyour ship"];
+  let quoteB = ["let them go", 
+                "if only I know.", 
+                "Where is your code.", 
+                "Don't give up", 
+                "read carefully"];
+  let quoteC = ["did you solve it?", 
+                "I forget to leave the key",
+                "after lunch",
+                "if it is possible",
+                "trust the others"
+];
+
 
 
 };
+console.log("hello");
+
