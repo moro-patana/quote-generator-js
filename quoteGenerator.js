6326ab9b6344d6448ab8652cf9851a8ceca20c66
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-09 15:06:40 +0300

    I commented the first console.log

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -23,13 +23,16 @@ let randomQuoteB = Math.floor(Math.random() * quoteB.length);
 let randomQuoteC = Math.floor(Math.random() * quoteC.length);
 
 let newQuote = `${quoteA[randomQuoteA]} ${quoteB[randomQuoteB]} ${quoteC[randomQuoteC]}`;
-console.log(newQuote);
+//console.log(newQuote);
 
 //1. use prompt to ask the user how many times we want to generate a random quote
+ //3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!//
+
 let number = 0;
 while (number < 1 || number > 5) {
  number = prompt("Enter the number of times we want to generate a random: ");
- //3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!//
+ //4. refactor our code into a function, and put the function call inside the loop
+
  if (number >= 1 && number <= 5) {
      for(let i = 0; i < number; i++) {
        const quoteGenerator = () => {
@@ -46,3 +49,6 @@ console.log(newQuote);
 
 }
 }
+//2. check if the prompt give us something between 1 and 5
+//4.1 generate random numbers
+//4.2 show the generated quotes//
