bb7a971f622496f0a219dea7ca3cd6a7f638d1f9
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-09 14:34:01 +0300

    I created for loop and factor my code into a function

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -27,14 +27,29 @@ console.log(newQuote);
 
 //1. use prompt to ask the user how many times we want to generate a random quote
 
-let randomTimes = prompt("Enter the number of times we want to generate a random");
-let numberOfQuotesGenerated = Math.floor(Math.random() * 5);
+const number = prompt("Enter the number of times we want to generate a random: ");
+if (number >= 1 && number <= 5) {
+     for(let i = 0; i < number; i++) {
+       const quoteGenerator = () => {
+          let randomQuoteA = Math.floor(Math.random() * quoteA.length);
+          let randomQuoteB = Math.floor(Math.random() * quoteB.length);
+          let randomQuoteC = Math.floor(Math.random() * quoteC.length);
+          let newQuote = `${quoteA[randomQuoteA]} ${quoteB[randomQuoteB]} ${quoteC[randomQuoteC]}`;
+console.log(newQuote);
+
+}
+
+ quoteGenerator();
+}
+
+}
+
 //2. check if the prompt give us something between 1 and 5
 
 
 //3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
 for(let i = 0; i < newQuote.legth; i++) {
-    
+
 }
 console.log(newQuote)
 //4. refactor our code into a function, and put the function call inside the loop
